14b75b4cfc6b75f0aad3b20735880a13713c8a2b
llanunes <98404361+llanunes@users.noreply.github.com>
2022-08-28 23:25:03 -0300

    Update app.js

--- exercicio01/app.js
+++ exercicio01/app.js
@@ -6,116 +6,129 @@
  * Versao:                  1.0.22
  */
 
- const {inserirDados, statusAluno} = require('./modulos/dadosAluno');
- const {media} = require('./modulos/dadosAluno');
- const {classificacao} = require('./modulos/dadosAluno');
+const { calcMedia } = require('./modulo/funcoes');
+const { verStatusAluno } = require('./modulo/funcoes');
+const { validarExame } = require('./modulo/funcoes');
+const { validarStatusExame } = require('./modulo/funcoes');
+const { relatorio } = require('./modulo/funcoes');
 
- var readline = require('readline');
- var entradaDados = readline.createInterface({
-     input:  process.stdin,
-     output: process.stdout
- });
- 
- entradaDados.question('\nInsira o sexo do(a) aluno(a) [Mascuino][Feminino]: ', function (generoAluno) {
-     let sexoAluno = generoAluno.toString().toLowerCase();
-     let prefixoGeneroAluno;
-     if (sexoAluno == '') {
-         console.log('\nERRO: É necessario inserir o sexo do aluno\n');
-         process.exit(1);
-     } else if (sexoAluno == 'masculino') {
-         prefixoGeneroAluno = 'o';
-     } else if (sexoAluno == 'feminino') {
-         prefixoGeneroAluno = 'a';
-     } else if (sexoAluno != 'feminino' || sexoProfessor != 'masculino'){
-        console.log('\nERRO: Insira um sexo válido.')
+var readline = require('readline');
+var entradaDados = readline.createInterface({
+    input:  process.stdin,
+    output: process.stdout
+});
+
+console.log("\n---SISTEMA---\n")
+
+entradaDados.question('\nInsira o sexo do(a) aluno(a): ', function (generoAluno) {
+    let sexoAluno = generoAluno.toString().toLowerCase();
+    let prefixoGeneroAluno;
+    if (sexoAluno == 'masculino') {
+        prefixoGeneroAluno = 'o';
+    } else if (sexoAluno == 'feminino') {
+        prefixoGeneroAluno = 'a';
+    } else {
+        console.log('\nERRO: É necessario inserir o sexo do(a) aluno(a)');
         process.exit(1);
-      }
-     entradaDados.question('Insira o nome d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ', function (nome) {
-         let nomeAluno = nome;
-         if (nomeAluno == '') {
-             console.log('ERRO: Insira o nome d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno);
-             process.exit(1);
-         }
-         entradaDados.question('Insira o sexo do(a) professor(a) [Mascuino][Feminino]: ', function (generoProfessor) {
-             let sexoProfessor = generoProfessor.toString().toLowerCase();
-             let prefixoGeneroProfessor;
-             let prefixoGenero;
-             if (sexoProfessor == '') {
-                 console.log('\nERRO:  É necessario inserir o sexo do professor\n');
-                 process.exit(1);
-             } else if (sexoProfessor == 'masculino') {
-                 prefixoGeneroProfessor = 'or';
-                 prefixoGenero = 'o';
-             } else if (sexoProfessor == 'feminino') {
-                 prefixoGeneroProfessor = 'ora';
-                 prefixoGenero = 'a';
-              } else if (sexoProfessor != 'feminino' || sexoProfessor != 'masculino'){
-                console.log('\nERRO: Insira um sexo válido.')
+    }
+    entradaDados.question('Insira o nome d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ', function (nome) {
+        let nomeAluno = nome;
+        if (nomeAluno == '') {
+            console.log('ERRO: Insira o nome d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno);
+            process.exit(1);
+        }
+        entradaDados.question('Insira o sexo do(a) professor(a): ', function (generoProfessor) {
+            let sexoProfessor = generoProfessor.toString().toLowerCase();
+            let prefixoGeneroProfessor;
+            let prefixoGenero;
+            if (sexoProfessor == 'masculino') {
+                prefixoGeneroProfessor = 'or';
+                prefixoGenero = 'o';
+            } else if (sexoProfessor == 'feminino') {
+                prefixoGeneroProfessor = 'ora';
+                prefixoGenero = 'a';
+            } else {
+                console.log('\nERRO: É necessario inserir o sexo do professor\n');
                 process.exit(1);
-              }
-
-             entradaDados.question('Insira o nome d' + prefixoGenero + ' profess' + prefixoGeneroProfessor + ': ', function (nome) {
-                 let nomeProfessor = nome;
-                 if (nomeProfessor == '') {
-                     console.log('ERRO: Insira o nome d' + prefixoGenero + ' profess' + prefixoGeneroProfessor);
-                     process.exit(1);
-                 }
-                 entradaDados.question('Insira o nome do curso: ', function (curso) {
-                     let nomeCurso = curso;
-                     if (nomeCurso == '') {
-                         console.log('\nERRO: Insira o nome do curso');
-                         process.exit(1);
-                     }
-                     entradaDados.question('Insira o nome da disciplina: ', function (disciplina) {
-                         let nomeDisciplina = disciplina;
-                         if (nomeDisciplina == '') {
-                             console.log('\nERRO: Insira o nome da disciplina');
-                             process.exit(1);
-                         }
-                        entradaDados.question('Insira a primeira nota: ', function (primeiraNota) {
-                            let nota1 = primeiraNota;
-                            if ( nota1 == '') {
-                                console.log('\nERRO: Insira o valor da primeira nota ');
-                                process.exit(1);
-                            } else if( isNaN (nota1) || nota1 < 0 || nota1 > 100){
-                                console.log('\nERRO: Insira uma nota válida.')
+            }
+            entradaDados.question('Insira o nome d' + prefixoGenero + ' profess' + prefixoGeneroProfessor + ': ', function (nome) {
+                let nomeProfessor = nome;
+                if (nomeProfessor == '') {
+                    console.log('ERRO: Insira o nome d' + prefixoGenero + ' profess' + prefixoGeneroProfessor);
+                    process.exit(1);
+                }
+                entradaDados.question('Insira o nome do curso: ', function (curso) {
+                    let nomeCurso = curso;
+                    if (nomeCurso == '') {
+                        console.log('\nERRO: É necessário inserir o nome do curso');
+                        process.exit(1);
+                    }
+                    entradaDados.question('Insira o nome da disciplina: ', function (disciplina) {
+                        let nomeDisciplina = disciplina;
+                        if (nomeDisciplina == '') {
+                            console.log('\nERRO: É necessário inserir o nome da disciplina');
+                            process.exit(1);
+                        }
+                        
+                        entradaDados.question('\nInsira a primeira nota d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ', function (nota) {
+                            let primeiraNota = parseFloat(nota);
+                            if (isNaN(primeiraNota) || primeiraNota < 0 || primeiraNota > 100) {
+                                console.log('\nERRO: Insira uma nota valida');
                                 process.exit(1);
                             }
-                            entradaDados.question('Insira a segunda nota: ', function(segundaNota) {
-                                let nota2 = segundaNota;
-                                if (nota2 == ''){
-                                    console.log('\nERRO: Insira o valor da segunda nota.')
-                                    process.exit(1);
-                                } else if(isNaN(nota2) || nota2 < 0 || nota2 > 100){
-                                    console.log('\nERRO: Insira uma nota válida.')
+                            entradaDados.question('Insira a segunda nota d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ', function (nota) {
+                                let segundaNota = parseFloat(nota);
+                                if (isNaN(segundaNota) || segundaNota < 0 || segundaNota > 100) {
+                                    console.log('\nERRO: Insira uma nota valida');
                                     process.exit(1);
                                 }
-                                entradaDados.question('Insira a terceira nota: ', function(terceiraNota) {
-                                    let nota3 = terceiraNota;
-                                    if (nota3 == ''){
-                                        console.log('\nERRO: Insira o valor da terceira nota.')
-                                        process.exit(1);
-                                    } else if(isNaN(nota3) || nota3 < 0 || nota3 > 100){
-                                        console.log('\nERRO: Insira uma nota válida.')
+                                entradaDados.question('Insira a terceira nota d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ', function (nota) {
+                                    let terceiraNota = parseFloat(nota);
+                                    if (isNaN(terceiraNota) || terceiraNota < 0 || terceiraNota > 100) {
+                                        console.log('\nERRO: Insira uma nota valida');
                                         process.exit(1);
                                     }
-                                    entradaDados.question('Insira a quarta nota: ', function(quartaNota){
-                                        let nota4 = quartaNota
-                                        if(nota4 == ''){
-                                            console.log('\nERRO: Insira o valor da quarta nota.')
-                                            process.exit(1);
-                                        } else if (isNaN(nota4) || nota4 < 0 || nota4 > 100){
-                                            console.log('\nERRO: Insira uma nota válida.')
+                                    entradaDados.question('Insira a quarta nota d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ', function (nota) {
+                                        let media = 0;
+                                        let mediaFinal;
+                                        let statusAluno;
+                                        let statusAlunoExame
+                                        let notaExame;
+                                        let mediaExame;
+                                        let quartaNota = parseFloat(nota);
+                                        if (isNaN(quartaNota) || quartaNota < 0 || quartaNota > 100) {
+                                            console.log('\nERRO: Insira uma nota valida');
                                             process.exit(1);
                                         }
-                                       console.log (prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + nomeAluno + ' está:' + media(nota1, nota2, nota3, nota4, media));
+                                        media = calcMedia(primeiraNota, segundaNota, terceiraNota, quartaNota, media);
+                                        statusAluno = verStatusAluno(media, statusAluno);
+
+                                        if (statusAluno == 'Aprovado' || statusAluno == 'Reprovado') {
+                                            mediaFinal = media;
+                                            console.log('\nMedia: ' + media);
+                                            console.log('Status d' + prefixoGeneroAluno + ' Alun' + prefixoGeneroAluno + ': ' + statusAluno);
+                                            relatorio(nomeAluno, nomeProfessor, nomeCurso, nomeDisciplina, primeiraNota, segundaNota, terceiraNota, quartaNota, notaExame, media, mediaFinal, verStatusAluno, statusAlunoExame, prefixoGeneroAluno, prefixoGeneroProfessor);
+                                            process.exit(1);
+                                        } else {
+                                            console.log('\nStatus d' + prefixoGeneroAluno + ' Alun' + prefixoGeneroAluno + ': ' + statusAluno);
+                                            entradaDados.question('Insira a nota que ' + prefixoGeneroAluno + ' alun' + '' + prefixoGeneroAluno + '' + nomeAluno + ' obteve no exame: ', function (nota) {
+                                                notaExame = nota;
+
+                                                mediaExame = validarExame(media, notaExame);
+                                                mediaFinal = mediaExame;
+                                                statusAlunoExame = validarStatusExame(mediaExame, statusAlunoExame);
+
+                                                relatorio(nomeAluno, nomeProfessor, nomeCurso, nomeDisciplina, primeiraNota, segundaNota, terceiraNota, quartaNota, notaExame, media, mediaFinal, statusAluno, statusAlunoExame, prefixoGeneroAluno, prefixoGeneroProfessor);
+                                                process.exit(1);
+                                            });
+                                        }
                                     });
-                                });   
-                            });    
-                        });    
-                     });
-                 });
-             });
-         });
-     });
- });
+                                });
+                            });
+                        });
+                    });
+                });
+            })
+        });
+    });
+});
