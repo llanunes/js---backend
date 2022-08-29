06845e01576c400e0039e5f99b6102f87be927f6
llanunes <98404361+llanunes@users.noreply.github.com>
2022-08-28 23:23:48 -0300

    Update dadosAluno.js

--- exercicio01/modulos/dadosAluno.js
+++ exercicio01/modulos/dadosAluno.js
@@ -1,5 +1,5 @@
 
-const media = function (primeiraNota, segundaNota, terceiraNota, quartaNota, media) {
+const calcMedia = function (primeiraNota, segundaNota, terceiraNota, quartaNota, media) {
     let nota1 = primeiraNota;
     let nota2 = segundaNota;
     let nota3 = terceiraNota;
@@ -9,7 +9,7 @@ const media = function (primeiraNota, segundaNota, terceiraNota, quartaNota, med
     return media;
 }
 
-const statusAluno = function (media, statusAluno){
+const verStatusAluno = function (media, statusAluno){
     if(media < 50){
         statusAluno = 'reprovado.';
     }else if(media < 70){
@@ -21,8 +21,36 @@ const statusAluno = function (media, statusAluno){
     return statusAluno;
 }
 
-module.exports = {
-    media, 
-    statusAluno
+const validarExame = function (media, nota) {
+    let notaMedia = parseFloat(media);
+    let notaExame = parseFloat(nota);
+
+    let mediaExame = (notaMedia + notaExame) / 2;
+
+    return mediaExame;
 }
 
+const validarStatusExame = function (mediaExame, statusAluno) {
+    if (mediaExame < 60) {
+        statusAluno = 'Reprovado!';
+    } else {
+        statusAluno = 'Aprovado através do exame!';
+    }
+    return statusAluno;
+}
+
+const relatorio = function (nomeAluno, nomeProfessor, nomeCurso, nomeDisciplina, nota1, nota2, nota3, nota4, notaExame, media, mediaFinal, statusAluno, statusAlunoExame, prefixoGeneroAluno, prefixoGeneroProfessor) {
+    if (statusAluno == 'Exame') {
+        console.log('\n' + prefixoGeneroAluno.toUpperCase() + ' alun' + prefixoGeneroAluno + ' ' + nomeAluno + ' foi ' + statusAlunoExame + ' na disciplina ' + nomeDisciplina + '.' + '\nCurso: ' + nomeCurso + '\nProfess' + prefixoGeneroProfessor + ': ' + nomeProfessor + '\nNotas d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ' + nota1 + ', ' + nota2 + ', ' + nota3 + ', ' + nota4 + ', ' + notaExame + '\nMédia final: ' + media + '\nMédia final do exame: ' + mediaFinal + '\n');
+    } else {
+        console.log('\n' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ' ' + nomeAluno + ' foi ' + statusAluno + ' na disciplina ' + nomeDisciplina + '.' + '\nCurso: ' + nomeCurso + '\nProfess' + prefixoGeneroProfessor + ': ' + nomeProfessor + '\nNotas d' + prefixoGeneroAluno + ' alun' + prefixoGeneroAluno + ': ' + nota1 + ', ' + nota2 + ', ' + nota3 + ', ' + nota4 + '\nMédia final: ' + media + '\n');
+    }
+}
+
+module.exports = {
+    calcMedia, 
+    verStatusAluno,
+    relatorio,
+    validarExame,
+    validarStatusExame
+}
